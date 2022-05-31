636b7c4a922b56b9af581c4673c0f1ea9e44a989
simon <simon.weckler@mnet-online.de>
2022-05-31 15:52:01 +0200

    feat: sensor data can be read (theoretically)

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import datetime
-import random;
+import random
+from time import sleep;
+import adafruit_dht;
+import board;
+import SI1145.SI1145 as SI1145;
 
 fileHandler = open("data.csv", "a")
 
-headings = ["Time", "Temperature", "Humidity", "UV Index"]
+READING_INTERVAL = 2.0
+
+htSensor = adafruit_dht.DHT22(board.D18)
+uvSensor = SI1145.SI1145()
+
+headings = ["Time", "Temperature", "Humidity", "UV", "IR", "Visible"]
 
 def writeRow(values):
     if(len(values) != len(headings)):
@@ -22,12 +31,48 @@ def sampleRow():
     time = datetime.datetime.now()
     temperature = random.uniform(-50, 20)
     humidity = random.uniform(10, 50)
-    uvIndex = random.uniform(0, 13)
-    return [time, temperature, humidity, uvIndex]
+    uv = random.uniform(0, 13)
+    ir = random.uniform(0, 100)
+    visible = random.uniform(0, 20)
+    return [time, temperature, humidity, uv, ir, visible]
 
-writeRow(headings)
+def readHumidityAndTemperature():
+    try:
+        temperature = htSensor.temperature
+        humidity = htSensor.humidity
+    except RuntimeError as error:
+        print(error)
+        sleep(2)
+    except Exception as error:
+        htSensor.exit()
+        raise error
+    
+    return [temperature, humidity]
 
-for i in range(0, 100):
-    writeRow(sampleRow())
+def readIRVisibleUV():
+    try:
+        vis = uvSensor.readVisible()
+        uv = uvSensor.readUV()
+        ir = uvSensor.readIR()
+    except RuntimeError as error:
+        print(error)
+        sleep(2)
+    except Exception as error:
+        raise error
+
+    return [uv, ir, vis]
+
+def writeSample():
+    for i in range(0, 100):
+        writeRow(sampleRow())
+
+def writeSensorData():
+    while True:
+        tempAndHum = readHumidityAndTemperature()
+        uvIRVisible = readIRVisibleUV()
+        writeRow(tempAndHum + uvIRVisible)
+        sleep(READING_INTERVAL)
+
+writeSample()
 
 fileHandler.close();
